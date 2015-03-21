2ff1912ba7de4ce16cd6b6011a8c98c49ad09b2a
Julio Lopez <ljuliom@gmail.com>
2015-03-21 16:49:58 -0500

    aplying markdown in sent text

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "net/http"
+import (
+    "net/http"
+    "github.com/russross/blackfriday"
+    )
 
-func main(){
-  http.ListenAndServe(":8080", http.FileServer(http.Dir(".")))
+func main() {
+    http.HandleFunc("/markdown", GenerateMarkdown)
+    http.Handle("/", http.FileServer(http.Dir("public")))
+    http.ListenAndServe(":8080", nil)
+}
+
+func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
+    markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))
+    rw.Write(markdown)
 }
