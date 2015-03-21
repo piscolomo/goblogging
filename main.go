9cf4537b90f29ddc7bf65bf32688028ff68a1160
Julio Lopez <ljuliom@gmail.com>
2015-03-21 16:56:08 -0500

    moving to heroku

--- main.go
+++ main.go
@@ -2,16 +2,22 @@ package main
 
 import (
     "net/http"
+    "os"
     "github.com/russross/blackfriday"
     )
 
 func main() {
-    http.HandleFunc("/markdown", GenerateMarkdown)
-    http.Handle("/", http.FileServer(http.Dir("public")))
-    http.ListenAndServe(":8080", nil)
+  port := os.Getenv("PORT")
+  if port == "" {
+    port = "8080"
+  }
+
+  http.HandleFunc("/markdown", GenerateMarkdown)
+  http.Handle("/", http.FileServer(http.Dir("public")))
+  http.ListenAndServe(":8080", nil)
 }
 
 func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
-    markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))
-    rw.Write(markdown)
+  markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))
+  rw.Write(markdown)
 }
